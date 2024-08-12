e507fca9dfaa3c577392cc6c48665d397404b003
Ashok Suthar <82142415+ashoksuthar9549@users.noreply.github.com>
2024-08-12 18:37:47 +0530

    Add files via upload

---
+++ Section 12(new)/MEAN Project/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,53 @@
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private destroyRef = inject(DestroyRef);
+  private placesService = inject(PlacesService);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription =  this.placesService.loadAvailablePlaces()
+      .subscribe({
+        next: (places) => {
+          console.log(places); 
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace)
+    .subscribe({
+      next: (resData) => console.log(resData)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+}
